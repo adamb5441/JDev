767f263a93c4dff2f057980e011cc02fef0af466
Adam <adamb5441@gmail.com>
2020-01-20 18:44:11 -0500

    first commit

---
+++ jdev/src/components/Editor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-jsoneditor v-model="json" :options="options" :plus="true" height="1000px" @error="onError" />
+  </div>
+</template>
+
+<script>
+import VJsoneditor from 'v-jsoneditor/src/index'
+
+export default {
+  name: 'Editor',
+    components: {
+        VJsoneditor
+    },
+    data() {
+        return {
+            json: {
+                "hello": "vue"
+            },
+            options: {mode: 'code'}
+        }
+    },
+    methods: {
+        onError() {
+            console.log('error')
+        }
+    }
+};
+</script>
---
+++ jdev/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Stage from '../views/Stage'
+import Login from '../views/Login'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Stage
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
